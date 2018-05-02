77fc7b80a6d225722b4802505cd560ec0b564957
ChristofSeiler <christof.seiler@gmail.com>
2018-05-02 16:42:31 -0700

    Update compile_R.bash

--- compile_R.bash
+++ compile_R.bash
@@ -7,10 +7,13 @@ module load gcc
 wget https://ftp.gnu.org/gnu/readline/readline-7.0.tar.gz
 cd readline-7.0/
 ./configure --prefix=/home/groups/sph/
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/groups/sph/lib"
+export CPPFLAGS='-I/home/groups/sph/include'
+export LDFLAGS='-L/home/groups/sph/lib'
+cd ..
 
 # R: download source and install
 wget https://cran.r-project.org/src/base/R-3/R-3.5.0.tar.gz
 tar -xvf R-3.5.0.tar.gz
 cd R-3.5.0/
 ./configure --prefix=/home/groups/sph/R/3.5.0 --enable-R-shlib --with-blas --with-lapack
+cd ..
